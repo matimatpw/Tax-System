24c6a1cdead33ba96fdb108a09e603842973590d
Mateusz Matukiewicz <01178563@pw.edu.pl>
2023-05-17 21:32:24 +0200

    Add project files.

---
+++ PROI_PODATKI_UnitTests/PROI_PODATKI_UnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PROIPODATKIUnitTests
+{
+	TEST_CLASS(PROIPODATKIUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
