eeb7b2252c162c1f5bcfa68a0f695ec2cc0cb507
Przemysław Walecki <01178613@pw.edu.pl>
2023-05-31 16:29:24 +0200

    updated tax

--- PROI_PODATKI_Lib/Tax.h
+++ PROI_PODATKI_Lib/Tax.h
@@ -1,33 +1,34 @@
 #pragma once
 class Tax
 {
+    float income;
 public:
-	virtual float calculate_tax();
+    virtual float calculate_tax(float inc) = 0;
 };
 
 // FIRMA-----
 class Vat : public Tax {
+
 public:
-	float calculate_tax();
+    float calculate_tax(float inc);
 };
 
 class Cit : public Tax {
-	float calculate_tax();
+    float calculate_tax(float inc);
 };
 
 
 // Osoba FIzyczna-----
 class Pit : public Tax {
-	float calculate_tax();
+    float calculate_tax(float inc);
 };
 
 class Pon : public Tax { //Podatek Od Nieruchomosci
-	float calculate_tax();
+    float calculate_tax(float inc);
 };
 
 
 //FIrma i Osoba Fizyczna
 class Zus : public Tax {
-	float calculate_tax();
+    float calculate_tax(float inc);
 };
-
